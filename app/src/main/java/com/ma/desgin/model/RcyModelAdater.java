139e3e4ca281535b3aed9fbf09f767eab7820903
sharkmabin <814233092@qq.com>
2017-03-29 16:58:34 +0800

    第二次创建模式列表

--- app/src/main/java/com/ma/desgin/model/RcyModelAdater.java
+++ app/src/main/java/com/ma/desgin/model/RcyModelAdater.java
@@ -14,6 +14,7 @@ public class RcyModelAdater {
         private TextView textView ;
         public ViewHolder(View itemView) {
             super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.tvModelName);
 
         }
     }
